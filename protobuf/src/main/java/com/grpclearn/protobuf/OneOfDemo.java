9bca17a492a88c88cf922023ccd8dd4e45bea566
iss211 <Glen.Abraham@immcossi.com>
2023-05-15 10:20:17 +0530

    tag comments

--- protobuf/src/main/java/com/grpclearn/protobuf/OneOfDemo.java
+++ protobuf/src/main/java/com/grpclearn/protobuf/OneOfDemo.java
@@ -8,11 +8,23 @@ public class OneOfDemo {
 	public static void main(String[] args) {
 		EmailCredentials emailCred = EmailCredentials.newBuilder().setEmail("glen@gmail.com").setPassword("password").build();
 		PhoneOtp phoneOtp = PhoneOtp.newBuilder().setNumber(1234567890).setCode(456).build();
-		Credentials cred = Credentials.newBuilder().setPhoneMode(phoneOtp).build();
+		Credentials cred = Credentials.newBuilder().setPhoneMode(phoneOtp).setEmailMode(emailCred).build();
 		login(cred);
 	}
 	
 	private static void login(Credentials credentials) {
-		System.out.println(credentials.getPhoneMode());
+		switch (credentials.getModeCase()) {
+		case EMAILMODE:{
+			System.out.println(credentials.getEmailMode());
+			break;
+		}
+		case PHONEMODE:{
+			System.out.println(credentials.getPhoneMode());
+			break;
+		}
+
+		default:
+			break;
+		}
 	}
 }
